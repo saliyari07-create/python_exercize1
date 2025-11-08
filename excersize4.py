767f0f9e5ab564d12902c8dabd3f62f82f1a9a16
saliyari07-create <saliyari07@gmail.com>
2025-11-08 22:07:51 +0330

    Add files via upload

---
+++ excersize4.py
@@ -0,0 +1,7 @@
+while True:
+  x=float(input('enter first  number:\n'))
+  y=float(input('enter second number:\n'))
+  z=float(input('enter third number:\n'))
+  print(max(x,y,z))
+  print(min(x,y,z))
+  
