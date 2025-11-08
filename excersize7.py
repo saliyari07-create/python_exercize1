5667deceddf96327bd3e9f13d781e539a5e0a914
saliyari07-create <saliyari07@gmail.com>
2025-11-08 22:56:08 +0330

    Add files via upload

---
+++ excersize7.py
@@ -0,0 +1,12 @@
+while True:
+  m=int(input('enter your mark:'))
+  if 14<=m<16:
+    print('C')
+  elif 16<=m<=18:
+    print('B')
+  elif 18<m<=20:
+    print('A')
+  else:
+    print('Error')
+         
+  
